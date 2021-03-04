9a0e9f464e3a82b0b2900ea8e8f8ac490897850f
黄亮 <h.love.pure@gmail.com>
2021-03-04 11:22:03 +0800

    feat: first commit

---
+++ geecache/lru/lru.go
@@ -0,0 +1,75 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, OnEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: OnEvicted,
+	}
+}
+
+// 查找
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, ok
+	}
+	return
+}
+
+// 移除最近最少访问的节点（队首）
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(value.Len()) + int64(len(key))
+	}
+
+	for c.maxBytes != 0 && c.nbytes > c.maxBytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len for test
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.15
+
+require (
+	geecache v0.0.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
+
+replace geecache => ./geecache
